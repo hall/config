cdbf3094e656853ee5f71a473742a2972624e59c
Bryton Hall <email@bryton.io>
2021-12-07 18:47:14 -0500

    add a few more firefox settings

--- home/programs/firefox/default.nix
+++ home/programs/firefox/default.nix
@@ -7,12 +7,15 @@
       id = 0;
       name = "default";
       settings = {
+        "browser.aboutConfig.showWarning" = false;
+        "browser.newtabpage.enabled" = false;
         "browser.search.defaultenginename" = "duckduckgo";
         "browser.startup.homepage" = "about:blank";
+        "browser.startup.page" = 3; # restore previous session
         "browser.tabs.drawInTitlebar" = false;
+        "browser.toolbars.bookmarks.visibility" = "never";
         "devtools.theme" = "dark";
         "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
-        "projectManager.git.baseFolders" = "~/src";
         "signon.rememberSignons" = false;
         "svg.context-properties.content.enabled" = true;
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
@@ -30,7 +33,6 @@
     # buster
     # bypass-paywalls
     # ipfs-companion
-    onepassword-password-manager
     anchors-reveal
     auto-tab-discard
     darkreader
