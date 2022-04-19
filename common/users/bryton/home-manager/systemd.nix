d14bd3829834ccf06ba74bdab4120b69b175beff
Bryton Hall <email@bryton.io>
2022-04-18 22:34:09 -0400

    auto-enable systemd user units

--- common/users/bryton/home-manager/systemd.nix
+++ common/users/bryton/home-manager/systemd.nix
@@ -1,17 +1,19 @@
 { pkgs, ... }:
 {
-  # TODO: how to enable?
-  user.services = {
-    gotify-desktop = {
-      Unit = {
-        Description = "Gotify Desktop";
-        After = "network.target";
-      };
-      Service = {
-        ExecStart = "${pkgs.gotify-desktop}/bin/gotify-desktop";
-      };
-      Install = {
-        WantedBy = [ "graphical.target" ];
+  user = {
+    startServices = "sd-switch";
+    services = {
+      gotify-desktop = {
+        Unit = {
+          Description = "Gotify Desktop";
+          After = "network.target";
+        };
+        Service = {
+          ExecStart = "${pkgs.gotify-desktop}/bin/gotify-desktop";
+        };
+        Install = {
+          WantedBy = [ "graphical.target" ];
+        };
       };
     };
   };
