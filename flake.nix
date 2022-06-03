e9a8ba968809ec9d5667ac0908d601343c197a1e
Bryton Hall <email@bryton.io>
2022-06-03 09:50:32 -0400

    update floke inputs

--- flake.nix
+++ flake.nix
@@ -1,12 +1,14 @@
 {
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    stable.url = github:nixos/nixpkgs/nixos-21.11;
+    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable; # 21.11
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nur.url = github:nix-community/nur;
-    home-manager.url = github:nix-community/home-manager; #/release-21.11;
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     musnix.url = github:musnix/musnix;
-    mach.url = github:davhau/mach-nix;
+    # mach.url = github:davhau/mach-nix;
     utils.url = github:gytis-ivaskevicius/flake-utils-plus/v1.3.1;
     # mobile.url = github:nixos/mobile-nixos/master;
   };
