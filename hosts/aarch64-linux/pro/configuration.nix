1e9546d0d5a87bee44d6903c3ce5eef78f771094
Bryton Hall <email@bryton.io>
2022-06-13 00:46:14 -0400

    add initial pinephone config

--- hosts/aarch64-linux/pro/configuration.nix
+++ hosts/aarch64-linux/pro/configuration.nix
@@ -3,79 +3,52 @@ let
   username = "bryton";
 in
 {
+  imports = [ ./hardware.nix ];
+
+  boot.loader.grub.enable = false;
   # numeric password is currently required to unlock a session
   # TODO change me!
-  users.users.${username}.hashedPassword = "$6$zActsdzv754qmpNR$TVgNLHx4/0Q3GIqirequckS252LvYFomx11IimP8uuk.soV8CQFIUDcjhhF7lHz5BurJZJLj/QlGOHZTYAX8R1";
-  users.users.root.hashedPassword = "$6$Jv0Cl55I5TN$BAwcCxOv7Yvy3z369jwFU7/x.TfUOCEvM6FVxsQOPWJEFgKYhZvsgDmvF3gb8dgOAntHvYHR8QF0obpezLx3v1";
-
-  hardware.opengl.enable = true;
-
-  systemd.defaultUnit = "graphical.target";
-  systemd.services.phosh = {
-    wantedBy = [ "graphical.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.phosh}/bin/phosh";
-      User = 1000;
-      PAMName = "login";
-      WorkingDirectory = "~";
-
-      TTYPath = "/dev/tty7";
-      TTYReset = "yes";
-      TTYVHangup = "yes";
-      TTYVTDisallocate = "yes";
-
-      StandardInput = "tty-fail";
-      StandardOutput = "journal";
-      StandardError = "journal";
-
-      UtmpIdentifier = "tty7";
-      UtmpMode = "user";
-
-      Restart = "always";
+  users.users = {
+    ${username} = {
+      isNormalUser = true;
+      group = "users";
+      password = "1234";
     };
+    root.password = "1234";
+    # geoclue.extraGroups = [ "networkmanager" ];
   };
 
-  # unpatched gnome-initial-setup is partially broken in small screens
-  services.gnome.gnome-initial-setup.enable = false;
-
-  programs.phosh.enable = true;
-
-  environment.etc."machine-info".text = lib.mkDefault ''
-    CHASSIS="handset"
-  '';
-
-  networking = {
-    # FIXME : configure usb rndis through networkmanager in the future.
-    # Currently this relies on stage-1 having configured it.
-    networkmanager.unmanaged = [ "rndis0" "usb0" ];
+  hardware = {
+    sensor.iio.enable = true;
   };
 
-  # Setup USB gadget networking in initrd...
-  mobile.boot.stage-1.networking.enable = lib.mkDefault true;
 
-  services.openssh = {
-    enable = true;
-    passwordAuthentication = false;
-    permitRootLogin = "no";
-    allowSFTP = false;
+  services = {
+    fwupd.enable = true;
+
+    xserver.desktopManager.phosh = {
+      enable = true;
+      user = username;
+      group = "users";
+    };
+
+    openssh = {
+      enable = true;
+      # passwordAuthentication = false;
+      # permitRootLogin = "no";
+      # allowSFTP = false;
+    };
+
+    # geoclue.enable = true;
+
   };
 
-  programs.mosh.enable = true;
+  environment.systemPackages = with pkgs; [
+    chatty
+    megapixels
+    gnome.gnome-terminal
+  ];
 
-  # Don't start it in stage-1 though.
-  # (Currently doesn't quit on switch root)
-  # mobile.boot.stage-1.ssh.enable = true;
+  programs.calls.enable = true;
 
-
-  # Ensures this demo rootfs is useable for platforms requiring FBIOPAN_DISPLAY.
-  mobile.quirks.fb-refresher.enable = true;
-
-  # Okay, systemd-udev-settle times out... no idea why yet...
-  # Though, it seems fine to simply disable it.
-  # FIXME : figure out why systemd-udev-settle doesn't work.
-  systemd.services.systemd-udev-settle.enable = false;
-
-  # Force userdata for the target partition. It is assumed it will not
-  # fit in the `system` partition.
-  mobile.system.android.system_partition_destination = "userdata";
 }
---
+++ hosts/aarch64-linux/pro/modules.nix
@@ -0,0 +1,3 @@
+[
+  (import "${flake.inputs.mobile}/lib/configuration.nix" { device = "pine64-pinephonepro"; })
+]
