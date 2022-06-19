14d582c7ebdbd8eda1d0ef99044a1062b154614e
Bryton Hall <email@bryton.io>
2022-06-19 19:54:05 -0400

    add hardware input

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = github:nixos/nixpkgs/nixos-22.05; # 21.11
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    hardware.url = github:NixOS/nixos-hardware/master;
     nur.url = github:nix-community/nur;
     home-manager = {
       url = github:nix-community/home-manager;
@@ -41,10 +42,12 @@
         ];
       };
 
-      hostDefaults.modules = [
-        ./common/configuration.nix
-        inputs.musnix.nixosModules.musnix
-      ];
+      hostDefaults = {
+        modules = [
+          ./common/configuration.nix
+          inputs.musnix.nixosModules.musnix
+        ];
+      };
 
       hosts = lib.mkHosts {
         inherit self;
