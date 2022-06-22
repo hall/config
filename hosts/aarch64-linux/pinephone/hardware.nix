3b7c0aad8464ed01e85d3a3d93fa96ac71c599fc
Bryton Hall <email@bryton.io>
2022-06-22 00:57:46 -0400

    [pinephone] add sdcard home filesystem

--- hosts/aarch64-linux/pinephone/hardware.nix
+++ hosts/aarch64-linux/pinephone/hardware.nix
@@ -14,11 +14,16 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    {
+  fileSystems = {
+    "/" = {
       device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
       fsType = "ext4";
     };
+    "/home" = {
+      device = "/dev/disk/by-label/home";
+      fsType = "ext4";
+    };
+  };
 
   swapDevices = [ ];
 
