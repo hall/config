290b78ca7cf6e2a0615d06b70326102f3d0c09aa
Bryton Hall <email@bryton.io>
2022-07-24 23:47:06 -0400

    [vscode] read multiple files in overlay

--- overlays/vscodium/default.nix
+++ overlays/vscodium/default.nix
@@ -1,9 +1,24 @@
 self: super:
 super.vscodium.overrideAttrs (prev: {
   postFixup = ''
-    # insert custom html before closing tag
-    ${super.pkgs.gnused}/bin/sed -i \
-      '/<\/html>/e ${super.pkgs.coreutils}/bin/cat ${./custom.html}' \
-      $out/lib/vscode/resources/app/out/vs/code/electron-browser/workbench/workbench.html
+    # append content from stdin to end of element tag
+    append() {
+      ${super.pkgs.gnused}/bin/sed -i "/<\/$1>/e ${super.pkgs.coreutils}/bin/cat -" $out/lib/vscode/resources/app/out/vs/code/electron-browser/workbench/workbench.html 
+    }
+
+    # TODO: don't disable all CSP
+    sed -i '/Content-Security-Policy/d' $out/lib/vscode/resources/app/out/vs/code/electron-browser/workbench/workbench.html 
+
+    find ${./custom} -type f | while read file; do 
+      case $file in
+        *.js)
+          # echo -e "<meta http-equiv=\"Content-Security-Policy\" content=\"script-src 'sha256-$(echo -e "\n$(cat $file)\n" | sha256sum | cut -d' ' -f1)'\">" | append head
+          echo -e "<script>\n$(cat $file)\n</script>" | append html
+        ;;
+        *.css)
+          echo -e "<style>\n$(cat $file)\n</style>" | append head
+        ;;
+      esac
+    done
   '';
 })
