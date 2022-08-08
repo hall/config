46ec589a2e87f9cb39c67df3d1cde3ec2e5d1bea
Bryton Hall <email@bryton.io>
2022-08-07 23:39:31 -0400

    [x12] update default hardware config

--- conf/hosts/linux/x86_64/hardware.nix
+++ conf/hosts/linux/x86_64/hardware.nix
@@ -1,18 +1,18 @@
 { config, lib, pkgs, modulesPath, ... }:
 {
-  imports =
-    [
-      (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
   boot = {
     consoleLogLevel = 0;
     loader = {
       timeout = lib.mkForce 0; # set to 10 in isoImage
       systemd-boot.enable = true;
+      generic-extlinux-compatible.enable = false;
     };
     initrd = {
-      availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "usbhid" "sd_mod" "sdhci_pci" ];
+      availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usbhid" ];
       kernelModules = [ "dm-snapshot" ];
       luks.devices."crypt".device = "/dev/disk/by-partlabel/crypt";
       verbose = false;
@@ -23,7 +23,7 @@
     #  options snd-intel-dspcfg dsp_driver=3
     #'';
     # kernelPackages = pkgs.linuxKernel.packages.linux_5_17;
-    kernelModules = with config.boot.kernelPackages; [ "kvm-intel" ];
+    kernelModules = [ "kvm-intel" ];
   };
 
 
@@ -36,12 +36,17 @@
       device = "/dev/disk/by-label/boot";
       fsType = "vfat";
     };
+    # "/nix/store" = {
+    #   device = "/nix/store";
+    #   fsType = "none";
+    #   options = [ "bind" ];
+    # };
   };
-  swapDevices = [
-    {
-      device = "/dev/vg/swap";
-    }
-  ];
+  swapDevices = [{
+    device = "/dev/vg/swap";
+  }];
 
+  # nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
