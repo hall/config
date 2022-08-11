86a9122dac0e0f11846178c016ae45bea26564ae
Bryton Hall <email@bryton.io>
2022-08-10 23:08:06 -0400

    [pkgs] only return packages which match host platform

--- conf/packages/default.nix
+++ conf/packages/default.nix
@@ -3,8 +3,10 @@ channels.nixpkgs.lib.trivial.pipe (lib.readDirNames ./.) [
   (map (pkg:
     {
       name = pkg;
-      value = channels.nixpkgs.callPackage ./${pkg} {};
+      value = channels.nixpkgs.callPackage ./${pkg} { };
     }
   ))
+  # remove unsupported packages
+  (builtins.filter (x: builtins.elem channels.nixpkgs.system x.value.meta.platforms))
   builtins.listToAttrs
 ]
