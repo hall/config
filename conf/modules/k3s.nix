aee68ffbca64e209c3bf6860fbe74e9d09ee828b
Bryton Hall <email@bryton.io>
2022-08-29 09:41:42 -0400

    [k3s] update fw, assume rp4

--- conf/modules/k3s.nix
+++ conf/modules/k3s.nix
@@ -8,20 +8,34 @@ in
   config = mkIf cfg.enable {
     boot = {
       growPartition = true;
+      loader = {
+        grub.enable = false;
+        generic-extlinux-compatible.enable = false;
+        raspberryPi = {
+          enable = true;
+          version = 4;
+        };
+      };
+
       kernelParams = [
-        "cgroup_memory=1"
         "cgroup_memory=1"
         "cgroup_enable=memory"
       ];
     };
 
-    networking.firewall.allowedTCPPorts = [
-      10250 # metrics server
-    ] ++ (lib.optionals (cfg.role == "server") [
-      6443 # k8s
-      2379 # etcd client requests
-      2380 # etcd peer communication
-    ]);
+    networking.firewall = {
+      allowedUDPPorts = [
+        53 # dns
+        8472 # cni (flannel vxlan)
+      ];
+      allowedTCPPorts = [
+        10250 # metrics server
+      ] ++ (lib.optionals (cfg.role == "server") [
+        6443 # k8s api
+        2379 # etcd client requests
+        2380 # etcd peer communication
+      ]);
+    };
 
     services = {
       k3s = {
@@ -31,6 +45,7 @@ in
           "--disable-network-policy"
           "--disable traefik"
           "--disable local-storage"
+          # "--disable servicelb"
           # coredns, servicelb, metrics-server
         ]);
         tokenFile = "/run/secrets/k3s";
@@ -45,6 +60,7 @@ in
       libcgroup
       k3s
       nfs-utils # longhorn RWX
+      openiscsi # longhorn
     ];
 
     fileSystems = {
@@ -55,6 +71,5 @@ in
     };
 
     age.secrets.k3s.file = ../secrets/k3s.age;
-
   };
 }
