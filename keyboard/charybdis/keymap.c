029595fb423a714084f2b9b666a8a8b6e7f05cfe
Bryton Hall <email@bryton.io>
2022-08-31 19:54:12 -0400

    [kb] disable auto pointer layer

--- keyboard/charybdis/keymap.c
+++ keyboard/charybdis/keymap.c
@@ -165,43 +165,42 @@ layer_state_t layer_state_set_kb(layer_state_t state) {
 }
 
 // pointer layer active after pointer movement
-#define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-#define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS 500
-#define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD 2
-static uint16_t auto_pointer_layer_timer = 0;
+// #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+// #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS 500
+// #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD 2
+// static uint16_t auto_pointer_layer_timer = 0;
+
+// #ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+// #    include "timer.h"
+// #endif 
 
 
-#ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-#    include "timer.h"
-#endif 
+// #ifdef POINTING_DEVICE_ENABLE
+// #    ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+// report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+//     if (abs(mouse_report.x) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD || abs(mouse_report.y) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD) {
+//         if (auto_pointer_layer_timer == 0) {
+//             layer_on(LAYER_POINTER);
+// #        ifdef RGB_MATRIX_ENABLE
+//             rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
+//             rgb_matrix_sethsv_noeeprom(HSV_GREEN);
+// #        endif  // RGB_MATRIX_ENABLE
+//         }
+//         auto_pointer_layer_timer = timer_read();
+//     }
+//     return mouse_report;
+// }
 
+// void matrix_scan_kb(void) {
+//     if (auto_pointer_layer_timer != 0 && TIMER_DIFF_16(timer_read(), auto_pointer_layer_timer) >= CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS) {
+//         auto_pointer_layer_timer = 0;
+//         layer_off(LAYER_POINTER);
+// #        ifdef RGB_MATRIX_ENABLE
+//         rgb_matrix_mode_noeeprom(RGB_MATRIX_STARTUP_MODE);
+// #        endif  // RGB_MATRIX_ENABLE
+//     }
+//     matrix_scan_user();
+// }
+// #    endif  // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 
-#ifdef POINTING_DEVICE_ENABLE
-#    ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    if (abs(mouse_report.x) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD || abs(mouse_report.y) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD) {
-        if (auto_pointer_layer_timer == 0) {
-            layer_on(LAYER_POINTER);
-#        ifdef RGB_MATRIX_ENABLE
-            rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
-            rgb_matrix_sethsv_noeeprom(HSV_GREEN);
-#        endif  // RGB_MATRIX_ENABLE
-        }
-        auto_pointer_layer_timer = timer_read();
-    }
-    return mouse_report;
-}
-
-void matrix_scan_kb(void) {
-    if (auto_pointer_layer_timer != 0 && TIMER_DIFF_16(timer_read(), auto_pointer_layer_timer) >= CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS) {
-        auto_pointer_layer_timer = 0;
-        layer_off(LAYER_POINTER);
-#        ifdef RGB_MATRIX_ENABLE
-        rgb_matrix_mode_noeeprom(RGB_MATRIX_STARTUP_MODE);
-#        endif  // RGB_MATRIX_ENABLE
-    }
-    matrix_scan_user();
-}
-#    endif  // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-
-#endif      // POINTING_DEVICE_ENABLE
+// #endif      // POINTING_DEVICE_ENABLE
