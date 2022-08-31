883f3ce9f60bc3115560f3b74fe9eafb11402d89
Bryton Hall <email@bryton.io>
2022-08-31 19:51:18 -0400

    init kodi module

--- conf/hosts/linux/x86_64/tv/configuration.nix
+++ conf/hosts/linux/x86_64/tv/configuration.nix
@@ -1,102 +1,49 @@
 # NUC11PAHi7, 88:AE:DD:05:C9:46, F2 to enter bios
 { lib, pkgs, flake, ... }:
-let
-  kodi = (pkgs.kodi-wayland.passthru.withPackages (k: with k; [
-    # youtube
-    # controller-topology-project
-    iagl
-    invidious
-    jellyfin
-    joystick
-    libretro
-    netflix
-    steam-library
-    # steam-launcher
-  ]));
-in
 {
-  imports = [flake.inputs.hardware.nixosModules.intel-nuc-8i7beh ];
-  boot = {
-    # kernelPackages = pkgs.linuxPackages_latest;
-    # model=dell-headset-multi
-    # extraModprobeConfig = ''
-    #   options snd slots=snd-hda-intel
-    #   options snd-hda-intel index=0,1
-    # '';
-    kernelModules = [ "snd-hda-intel" ];
-  };
-  networking = {
-    firewall = {
-      allowedTCPPorts = [
-        8080 # web ui (old)
-        # 80 # web ui
-        9090 # json-rpc
-      ];
-      allowedUDPPorts = [
-        9777 # event server
-      ];
-    };
-    interfaces.enp89s0.wakeOnLan.enable = true;
-  };
+  imports = [ flake.inputs.hardware.nixosModules.intel-nuc-8i7beh ];
 
-  # allow binding kodi to port 80
-  # security.wrappers.kodi = {
-  #   owner = flake.username;
-  #   group = "root";
-  #   capabilities = "cap_net_bind_service=+eip";
-  #   source = "${kodi.outPath}/lib/kodi/kodi.bin";
+  # router = {
+  #   enable = true;
+  #   internal = "enp89s0";
+  #   external = "enp0s2of0u4";
   # };
 
-  programs.steam.enable = true;
+  kodi.enable = true;
 
-  hardware = {
-    video.hidpi.enable = true;
-    firmware = with pkgs; [
-      firmwareLinuxNonfree
-    ];
-    bluetooth = {
-      enable = true;
-      disabledPlugins = [ "sap" ];
-    };
-    xpadneo.enable = true;
-    enableRedistributableFirmware = true;
-    # opengl = {
-    #   enable = true;
-    #   extraPackages = with pkgs; [
-    #     intel-media-driver
-    #     vaapiVdpau
-    #     libvdpau-va-gl
-    #   ];
-    # };
-  };
+  networking.interfaces.enp89s0.wakeOnLan.enable = true;
+
+  # boot = {
+  #   model=dell-headset-multi
+  #   extraModprobeConfig = ''
+  #     options snd slots=snd-hda-intel
+  #     options snd-hda-intel index=0,1
+  #   '';
+  #   kernelModules = [ "snd-hda-intel" ];
+  # };
+
+  # sound.enable = true;
+
+  # hardware = {
+  #   pulseaudio.enable = lib.mkForce true;
+  #   video.hidpi.enable = true;
+  #   firmware = with pkgs; [
+  #     firmwareLinuxNonfree
+  #   ];
+  #   opengl = {
+  #     enable = true;
+  #     extraPackages = with pkgs; [
+  #       intel-media-driver
+  #       vaapiIntel
+  #       vaapiVdpau
+  #       libvdpau-va-gl
+  #     ];
+  #   };
+  # };
 
   # nixpkgs.config.packageOverrides = pkgs: {
-  #   vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  #   vaapiIntel = pkgs.vaapiIntel.override { 
+  #     enableHybridCodec = true;
+  #   };
   # };
-
-  environment = {
-    systemPackages = with pkgs;[
-      libcamera
-      libva
-      sof-firmware
-      libcec
-    ];
-  };
-
-  users.users.${flake.username}.extraGroups = [
-    "audio"
-    "sound"
-    "video"
-    "lp"
-    "dialout" # cec
-  ];
-
-  services = {
-    upower.enable = true;
-    cage = {
-      enable = true;
-      user = flake.username;
-      program = "${kodi}/bin/kodi-standalone";
-    };
-  };
 }
---
+++ conf/modules/kodi.nix
@@ -0,0 +1,103 @@
+{ config, lib, pkgs, flake, ... }:
+let
+  name = "kodi";
+  cfg = config.${name};
+
+  kodi = (pkgs.kodi.passthru.withPackages (k: with k; [
+    youtube
+    # controller-topology-project
+    iagl
+    # invidious
+    jellyfin
+    joystick
+    # libretro
+    netflix
+    steam-library
+    # steam-launcher
+  ]));
+
+in
+{
+  options.${name} = {
+    enable = lib.mkEnableOption "media center";
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    programs.steam.enable = true;
+
+    hardware = {
+      xpadneo.enable = true;
+      bluetooth = {
+        enable = true;
+        disabledPlugins = [ "sap" ];
+      };
+    };
+
+    environment = {
+      systemPackages = with pkgs;[
+        libcamera
+        libva
+        sof-firmware
+        libcec
+        libva-utils
+      ];
+    };
+
+    networking.firewall = {
+      allowedTCPPorts = [
+        8080 # web ui
+        9090 # json-rpc
+      ];
+      allowedUDPPorts = [
+        9777 # event server
+      ];
+    };
+
+    # allow binding kodi to port 80
+    # security.wrappers.kodi = {
+    #   owner = flake.username;
+    #   group = "root";
+    #   capabilities = "cap_net_bind_service=+eip";
+    #   source = "${kodi.outPath}/lib/kodi/kodi.bin";
+    # };
+
+    # x11:
+    # - steam not fullscreen
+    # - shutdown hangs
+    # wayland:
+    # - no sound
+    # - kodi fails to start randomly
+    services = {
+      # pipewire.enable = lib.mkForce false;
+      upower.enable = true;
+
+      # cage = {
+      #   enable = true;
+      #   user = flake.username;
+      #   program = "${kodi}/bin/kodi-standalone";
+      # };
+
+      xserver = {
+        enable = true;
+        desktopManager.kodi = {
+          enable = true;
+          package = kodi;
+        };
+        displayManager.autoLogin = {
+          enable = true;
+          user = flake.username;
+        };
+      };
+    };
+
+    users.users.${flake.username}.extraGroups = [
+      "audio"
+      "sound"
+      "video"
+      # "lp"
+      "dialout" # cec
+    ];
+
+  };
+}
