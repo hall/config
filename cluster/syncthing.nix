5f0a0d8f1f48c828ed7038267a96f6a819362307
Bryton Hall <email@bryton.io>
2022-09-16 01:46:48 -0400

    cluster: init syncthing

---
+++ cluster/syncthing.nix
@@ -0,0 +1,29 @@
+{ kubenix, vars, ... }:
+vars.simple {
+  inherit kubenix;
+  image = "syncthing/syncthing:1.21";
+  port = 8384;
+  host = "sync";
+  persistence = {
+    stash.existingClaim = "stash-media";
+  };
+  values.service.nodeport = {
+    enabled = true;
+    type = "NodePort";
+    externalTrafficPolicy = "Local";
+    ports = {
+      listen = {
+        enabled = true;
+        port = 22000;
+        protocol = "TCP";
+        targetPort = 22000;
+      };
+      discovery = {
+        enabled = true;
+        port = 21027;
+        protocol = "UDP";
+        targetPort = 21027;
+      };
+    };
+  };
+}
