beeaafd790e34fab754e572a82265177c12a06b0
Bryton Hall <email@bryton.io>
2022-09-29 01:10:31 -0400

    work: add vscode jira extension

--- hosts/linux/x86_64/rigetti/users/bryton/home-manager/default.nix
+++ hosts/linux/x86_64/rigetti/users/bryton/home-manager/default.nix
@@ -57,15 +57,50 @@
 
     };
 
-    vscode.extensions = (with pkgs.vscode-extensions; [
-      hashicorp.terraform
-    ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "hcl";
-        publisher = "hashicorp";
-        version = "0.2.1";
-        sha256 = "sha256-5dBLDJ7Wgv7p3DY0klqxtgo2/ckAHoMOm8G1mDOlzZc=";
-      }
-    ];
+    vscode = {
+      userSettings = {
+        "atlascode.bitbucket.enabled" = false;
+        "atlascode.jira.statusbar.enabled" = false;
+        "atlascode.jira.startWorkBranchTemplate.customTemplate" = "{{{issueKey}}}";
+        "atlascode.jira.jqlList" =
+          let
+            query = q: "project = Infrastructure AND assignee = currentUser() AND resolution = Unresolved ${q} ORDER BY priority";
+          in
+          [
+            {
+              id = "b0972d31-32eb-4204-bcfc-aada2039305d";
+              siteId = "3d2cc213-b5ca-4763-8895-5914364c694e";
+              name = "sprint";
+              query = query ''AND sprint in openSprints()'';
+              enabled = true;
+              monitor = true;
+            }
+            {
+              id = "80fadf03-cb61-47e7-8f75-af20fed5abb3";
+              siteId = "3d2cc213-b5ca-4763-8995-5914364c694e";
+              name = "assigned";
+              query = query '''';
+              enabled = true;
+              monitor = true;
+            }
+          ];
+      };
+      extensions = (with pkgs.vscode-extensions; [
+        hashicorp.terraform
+      ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "hcl";
+          publisher = "hashicorp";
+          version = "0.2.1";
+          sha256 = "sha256-5dBLDJ7Wgv7p3DY0klqxtgo2/ckAHoMOm8G1mDOlzZc=";
+        }
+        {
+          name = "atlascode";
+          publisher = "atlassian";
+          version = "2.10.12";
+          sha256 = "sha256-6YAutknTJzCUwnZ6O4A8gAfCVJk3kpW05jacxA9j9/M=";
+        }
+      ];
+    };
   };
 }
