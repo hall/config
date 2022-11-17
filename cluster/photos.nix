a0676f8457300ceb90d81cf44f65ee13e4a8c417
Bryton Hall <email@bryton.io>
2022-11-17 18:16:59 -0500

    k8s: add photoprism with mariadb

--- cluster/photos.nix
+++ cluster/photos.nix
@@ -1,22 +1,47 @@
-{ kubenix, vars, ... }:
-vars.simple {
-  inherit kubenix;
-  image = "photoprism/photoprism:220901-bullseye";
-  port = 9999;
-  persistence = {
-    main = {
-      size = "30Gi";
-      # mountPath = "/photoprism";
-      nameOverride = "";
+{ kubenix, vars, flake, ... }:
+flake.lib.recursiveMerge [
+  (vars.simple {
+    inherit kubenix;
+    image = "photoprism/photoprism:221116-jammy";
+    port = 2342;
+    host = "photos";
+    persistence = {
+      # unmodified images
+      originals.size = "30Gi";
+      # db, cache, session, thumbnails, etc
+      storage.size = "10Gi";
     };
-    # config.mountPath = "/config";
-  };
-  values.env = {
-    PHOTOPRISM_PUBLIC = "false";
-    PHOTOPRISM_STORAGE_PATH = "/media/storage";
-    PHOTOPRISM_ORIGINALS_PATH = "/media/originals";
-    PHOTOPRISM_CONFIG_PATH = "/config";
-    # PHOTOPRISM_ADMIN_PASSWORD = "please-change";
-    # PHOTOPRISM_DATABASE_DRIVER = "mysql";
-  };
-}
+    values.env = {
+      PHOTOPRISM_AUTH_MODE = "public"; # disable auth
+      PHOTOPRISM_SPONSOR = "true";
+      PHOTOPRISM_STORAGE_PATH = "/storage";
+      PHOTOPRISM_ORIGINALS_PATH = "/originals";
+      PHOTOPRISM_DATABASE_DRIVER = "mysql";
+      PHOTOPRISM_DATABASE_SERVER = "mariadb:3306";
+      PHOTOPRISM_DATABASE_PASSWORD = vars.secret "/mariadb/photoprism";
+    };
+  })
+
+  {
+    helm.releases.mariadb = {
+      chart = kubenix.lib.helm.fetch {
+        repo = "https://groundhog2k.github.io/helm-charts/";
+        chart = "mariadb";
+        version = "0.6.4";
+        sha256 = "sha256-AF1oIYnzsO/Ool48cmjYGgZJE+dx1g2qFEia0WhyKiw=";
+      };
+      values = {
+        userDatabase = {
+          name = "photoprism";
+          user = "photoprism";
+          password = vars.secret "/mariadb/photoprism";
+        };
+        settings.rootPassword = vars.secret "/mariadb/root";
+        storage = {
+          className = "longhorn-static";
+          requestedSize = "8Gi";
+        };
+      };
+    };
+  }
+]
