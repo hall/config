7ea9ab887b16e52de6e7d48483303a100d5f0ef3
Bryton Hall <email@bryton.io>
2022-12-01 20:17:56 -0500

    stash: bump to 0.18.0

--- cluster/stash.nix
+++ cluster/stash.nix
@@ -1,7 +1,7 @@
 { kubenix, vars, ... }:
 vars.simple {
   inherit kubenix;
-  image = "stashapp/stash:v0.17.2";
+  image = "stashapp/stash:v0.18.0";
   port = 9999;
   persistence = {
     media = {
