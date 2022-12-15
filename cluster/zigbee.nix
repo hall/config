a54fe4736f98cdfc0722bf1f0d1496cf61061abd
Bryton Hall <email@bryton.io>
2022-12-14 22:01:16 -0500

    zigbee: bump to 1.28.4

--- cluster/zigbee.nix
+++ cluster/zigbee.nix
@@ -1,7 +1,7 @@
 { kubenix, vars, ... }:
 vars.simple {
   inherit kubenix;
-  image = "koenkk/zigbee2mqtt:1.28.2";
+  image = "koenkk/zigbee2mqtt:1.28.4";
   port = 8080;
   host = "zigbee";
   persistence = {
