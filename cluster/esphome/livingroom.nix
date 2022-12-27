11ebc6cee554d25c46f6f75f4b66a57d7ce1b7d0
Bryton Hall <email@bryton.io>
2022-12-27 01:13:30 -0500

    esphome: add tv light

--- cluster/esphome/livingroom.nix
+++ cluster/esphome/livingroom.nix
@@ -1,1 +1,18 @@
-{ ... }: { }
+{ ... }: {
+  tv-light = {
+    packages.temphumid = "!include .temphumid.yaml";
+    substitutions.name = "tv-light";
+    e131 = { }; # port 5568
+    light = [{
+      platform = "neopixelbus";
+      variant = "WS2812";
+      pin = "GPIO3"; # RX
+      num_leds = 146; # 26 x 47 rectangle
+      name = "tv";
+      method.type = "esp8266_dma";
+      effects = [{
+        e131.universe = 1;
+      }];
+    }];
+  };
+}
