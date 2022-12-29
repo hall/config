aadd90256b9c74457b019cc9560f7ac8dd74f887
Bryton Hall <email@bryton.io>
2022-12-29 01:00:19 -0500

    linux/x86 cleanup

--- hosts/linux/x86_64/configuration.nix
+++ hosts/linux/x86_64/configuration.nix
@@ -1,12 +1,6 @@
 { config, lib, pkgs, ... }:
 {
   boot = {
-    kernelPackages = pkgs.linuxPackages_5_19;
-    # extraModulePackages = with config.boot.kernelPackages; [ wireguard ];
-    #extraModprobeConfig = ''
-    #  options snd-intel-dspcfg dsp_driver=3
-    #'';
-
     consoleLogLevel = 0;
     loader = {
       timeout = lib.mkForce 0; # set to 10 in isoImage
@@ -18,7 +12,7 @@
       kernelModules = [ "dm-snapshot" ];
       verbose = false;
     };
-    kernelParams = [ "quiet" ]; #"udev.log_priority=3" ];
+    kernelParams = [ "quiet" ]; #"udev.log_priority=3"
     kernelModules = [ "kvm-intel" ];
   };
 
@@ -32,17 +26,11 @@
       device = "/dev/disk/by-label/boot";
       fsType = "vfat";
     };
-    # "/nix/store" = {
-    #   device = "/nix/store";
-    #   fsType = "none";
-    #   options = [ "bind" ];
-    # };
   };
   swapDevices = [{
     device = "/dev/disk/by-label/swap";
   }];
 
-  # nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode = true;
 }
