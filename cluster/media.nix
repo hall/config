e9bdad13a4e15eec341634b604aea4df9a4d72bf
Bryton Hall <email@bryton.io>
2023-01-13 17:52:24 -0500

    cluster: init jellyfin, radarr, and lidarr

--- cluster/media.nix
+++ cluster/media.nix
@@ -6,6 +6,7 @@ flake.lib.recursiveMerge [
   (vars.simple {
     inherit kubenix;
     image = "linuxserver/transmission:version-3.00-r6";
+    host = "downloads";
     port = 9091;
     persistence = {
       config = { };
@@ -18,8 +19,42 @@ flake.lib.recursiveMerge [
 
   (vars.simple {
     inherit kubenix;
+    image = "jellyfin/jellyfin:10.8.8";
+    host = "player";
+    port = 8096;
+    persistence = {
+      config.enabled = true;
+      media.existingClaim = pvc;
+    };
+  })
+
+  (vars.simple {
+    inherit kubenix;
     image = "linuxserver/sonarr:3.0.9";
+    host = "movies";
     port = 8989;
+    persistence = {
+      config.enabled = true;
+      media.existingClaim = pvc;
+    };
+  })
+
+  (vars.simple {
+    inherit kubenix;
+    image = "linuxserver/radarr:4.3.2";
+    host = "shows";
+    port = 7878;
+    persistence = {
+      config.enabled = true;
+      media.existingClaim = pvc;
+    };
+  })
+
+  (vars.simple {
+    inherit kubenix;
+    image = "linuxserver/lidarr:1.0.2";
+    host = "music";
+    port = 8686;
     persistence = {
       config.enabled = true;
       media.existingClaim = pvc;
