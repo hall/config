09160dfecd0fb81e4a3c7f64075efb3e28dccf88
Bryton Hall <email@bryton.io>
2023-02-01 21:23:47 -0500

    cad(keyboard-mount): close loop

--- cad/keyboard-mount/main.py
+++ cad/keyboard-mount/main.py
@@ -5,18 +5,27 @@ from pathlib import Path
 
 dir = Path(__file__).resolve().parent
 
+tent = 30
+height = 60
+
 obj = (
     cqkit.Ribbon("XY", [
         ("start", {"position": (0, 0), "direction": 0, "width": 5}),
         # length of surface mount
-        ("line", {"length": 60}),
+        ("line", {"length": 113.9}),
         ("arc", {"radius": 10, "angle": 90}),
-        # height
-        ("line", {"length": 60}),
+        # height on short end
+        ("line", {"length": height}),
         # tenting angle
-        ("arc", {"radius": 20, "angle": 90-30}),
+        ("arc", {"radius": 20, "angle": 90-tent}),
         # length under keyboard
         ("line", {"length": 120}),
+        # angle back up
+        ("arc", {"radius": 20, "angle": 90+tent}),
+        # height on long end
+        ("line", {"length": 110}),
+        # angle to close the loop
+        ("arc", {"radius": 20, "angle": 90}),
     ]).render().extrude(20)
 )
 
