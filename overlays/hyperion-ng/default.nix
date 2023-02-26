8ddf8e40ca10cd4bb77e632c39df9263ad0ca727
Bryton Hall <email@bryton.io>
2023-02-25 23:24:06 -0500

    overlays: add hyperion for newest version (2.0.15)

---
+++ overlays/hyperion-ng/default.nix
@@ -0,0 +1,12 @@
+self: super:
+super.hyperion-ng.overrideAttrs
+  (prev: rec {
+    version = "2.0.15";
+    src = super.fetchFromGitHub {
+      owner = "hyperion-project";
+      repo = "hyperion.ng";
+      rev = version;
+      sha256 = "LbR9BjNQ83QiUj7iKBupfCPPqRGm7ixdDWY4i75GMSI=";
+      fetchSubmodules = true;
+    };
+  })
