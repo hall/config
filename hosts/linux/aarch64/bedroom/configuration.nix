25e2d85d17e8a6f14fe34227a05fa7862b99cdc6
Bryton Hall <email@bryton.io>
2023-03-21 23:27:23 -0400

    fix dtoverlay apply on rpi 3 hosts

--- hosts/linux/aarch64/bedroom/configuration.nix
+++ hosts/linux/aarch64/bedroom/configuration.nix
@@ -1,49 +1,107 @@
-{ lib, flake, pkgs, config, ... }:
-{
-  # networking.firewall = {
-  #   allowedTCPPorts = [ 8080 ];
-  #   allowedUDPPorts = [ 8080 ];
-  # };
+{ lib, flake, pkgs, config, ... }: {
+  # $ cat /proc/device-tree/model 
+  #   Raspberry Pi 3 Model B
+  # https://www.hifiberry.com/docs/data-sheets/datasheet-amp2
 
-  # environment.systemPackages = with pkgs; [
-  #   sof-firmware
-  # ];
-  # hardware.raspberry-pi."4" = {
-  #   audio.enable = true;
-  # };
+  nixpkgs.overlays = [
+    (_final: prev: {
+      deviceTree.applyOverlays = prev.callPackage ../apply-overlays-dtmerge.nix { };
+    })
+  ];
+
   monitor.enable = true;
 
-  swapDevices = [{ device = "/swapfile"; size = 1024; }];
-  # sound.enable = lib.mkForce false;
+  users.users.${flake.username}.extraGroups = [ "audio" ];
 
-  # boot.kernelPackages = pkgs.linuxPackages_rpi4;
-  # hardware.deviceTree = {
-  #   enable = true;
-  #   overlays = [ "${config.boot.kernelPackages.kernel}/dtbs/overlays/hifiberry-dacplus.dtbo" ];
-  # };
+  hardware.deviceTree = {
+    enable = true;
+    filter = lib.mkForce "*-rpi-3-b.dtb";
+    overlays = [
+      # {
+      #   name = "hifiberry-dacplus";
+      #   dtboFile = "${config.boot.kernelPackages.kernel}/dtbs/overlays/hifiberry-dacplus.dtbo";
+      # }
+      {
+        name = "hifiberry-dacplus";
+        # https://github.com/raspberrypi/linux/blob/rpi-5.15.y/arch/arm/boot/dts/overlays/hifiberry-dacplus-overlay.dts
+        dtsText = ''
+          // Definitions for HiFiBerry DAC+
+          /dts-v1/;
+          /plugin/;
 
-  # force_eeprom_read=0
-  # dtparam=audio=off
-  # dtoverlay=vc4-fkms-v3d,audio=off
-  boot.loader = {
-    raspberryPi = {
-      enable = true;
-      version = 3;
-      firmwareConfig = ''
-        dtoverlay=hifiberry-dacplus
-      '';
-    };
+          / {
+          	compatible = "brcm,bcm2835";
+
+          	fragment@0 {
+          		target-path = "/";
+          		__overlay__ {
+          			dacpro_osc: dacpro_osc {
+          				compatible = "hifiberry,dacpro-clk";
+          				#clock-cells = <0>;
+          			};
+          		};
+          	};
+
+          	fragment@1 {
+          		target = <&i2s>;
+          		__overlay__ {
+          			status = "okay";
+          		};
+          	};
+
+          	fragment@2 {
+          		target = <&i2c1>;
+          		__overlay__ {
+          			#address-cells = <1>;
+          			#size-cells = <0>;
+          			status = "okay";
+
+          			pcm5122@4d {
+          				#sound-dai-cells = <0>;
+          				compatible = "ti,pcm5122";
+          				reg = <0x4d>;
+          				clocks = <&dacpro_osc>;
+          				AVDD-supply = <&vdd_3v3_reg>;
+          				DVDD-supply = <&vdd_3v3_reg>;
+          				CPVDD-supply = <&vdd_3v3_reg>;
+          				status = "okay";
+          			};
+          			hpamp: hpamp@60 {
+          				compatible = "ti,tpa6130a2";
+          				reg = <0x60>;
+          				status = "disabled";
+          			};
+          		};
+          	};
+
+          	fragment@3 {
+          		target = <&sound>;
+          		hifiberry_dacplus: __overlay__ {
+          			compatible = "hifiberry,hifiberry-dacplus";
+          			i2s-controller = <&i2s>;
+          			status = "okay";
+          		};
+          	};
+
+          	__overrides__ {
+          		24db_digital_gain =
+          			<&hifiberry_dacplus>,"hifiberry,24db_digital_gain?";
+          		slave = <&hifiberry_dacplus>,"hifiberry-dacplus,slave?";
+          		leds_off = <&hifiberry_dacplus>,"hifiberry-dacplus,leds_off?";
+          	};
+          };
+        '';
+      }
+    ];
   };
 
-  # sound = {
-  #   extraConfig = ''
-  #     defaults.pcm.card 0
-  #   '';
-  # };
+  boot = {
+    kernelPackages = pkgs.linuxPackages_rpi3;
+    loader.generic-extlinux-compatible.enable = true;
+  };
 
   systemd.services.snapclient = {
     wantedBy = [
-      "pipewire.service"
       "multi-user.target" # enable on boot
     ];
     after = [
--- hosts/linux/aarch64/office/configuration.nix
+++ hosts/linux/aarch64/office/configuration.nix
@@ -1,75 +1,125 @@
-{ flake, pkgs, config, ... }:
-{
+{ flake, pkgs, config, lib, ... }: {
+  # $ cat /proc/device-tree/model 
+  #   Raspberry Pi 3 Model B+
+  # https://www.hifiberry.com/docs/data-sheets/datasheet-miniamp
+
+  nixpkgs.overlays = [
+    (_final: prev: {
+      deviceTree.applyOverlays = prev.callPackage ../apply-overlays-dtmerge.nix { };
+    })
+  ];
+
   monitor.enable = true;
+
+  users.users.${flake.username}.extraGroups = [ "audio" ];
+
+  hardware.deviceTree = {
+    enable = true;
+    filter = lib.mkForce "*-rpi-3-b-plus.dtb";
+    overlays = [{
+      name = "hifiberry-dac";
+      # https://github.com/raspberrypi/linux/blob/rpi-5.15.y/arch/arm/boot/dts/overlays/hifiberry-dac-overlay.dts
+      dtsText = ''
+         // Definitions for HiFiBerry DAC
+         /dts-v1/;
+         /plugin/;
+
+         / {
+          compatible = "brcm,bcm2837";
+
+         	fragment@0 {
+         		target = <&i2s>;
+         		__overlay__ {
+         			status = "okay";
+         		};
+         	};
+
+         	fragment@1 {
+         		target-path = "/";
+         		__overlay__ {
+         			pcm5102a-codec {
+         				#sound-dai-cells = <0>;
+         				compatible = "ti,pcm5102a";
+         				status = "okay";
+         			};
+         		};
+         	};
+
+         	fragment@2 {
+         		target = <&sound>;
+         		__overlay__ {
+         			compatible = "hifiberry,hifiberry-dac";
+         			i2s-controller = <&i2s>;
+         			status = "okay";
+         		};
+         	};
+        };
+      '';
+    }];
+  };
 
   boot = {
     kernelPackages = pkgs.linuxPackages_rpi3;
-    loader = {
-      raspberryPi = {
-        enable = true;
-        version = 3;
-        firmwareConfig = ''
-          dtoverlay=hifiberry-dac
+    loader.generic-extlinux-compatible.enable = true;
+  };
+
+  services = {
+    snapserver = {
+      enable = true;
+      openFirewall = true;
+      streams = {
+        all = {
+          type = "meta";
+          location = "/turntable/spotify";
+        };
+        turntable = {
+          type = "tcp";
+          location = "0.0.0.0:49566";
+          query = {
+            sampleformat = "44100:16:2";
+          };
+        };
+        spotify = {
+          type = "librespot";
+          location = "${pkgs.librespot}/bin/librespot";
+          query = {
+            name = "spotify";
+            devicename = "home";
+            volume = "60";
+          };
+        };
+      };
+    };
+  };
+
+  age.secrets.spotify.file = ../../../../secrets/spotify.age;
+
+  systemd = {
+    services.snapserver.serviceConfig.EnvironmentFile = "/run/secrets/spotify";
+    user.services = {
+      snapcast-sink = {
+        wantedBy = [ "pipewire.service" ];
+        after = [ "pipewire.service" ];
+        bindsTo = [ "pipewire.service" ];
+        path = with pkgs; [
+          gawk
+          pulseaudio
+        ];
+        script = ''
+          pactl load-module module-pipe-sink file=/run/snapserver/pipewire sink_name=Snapcast format=s16le rate=48000
         '';
       };
-    };
-  };
 
-  services.snapserver = {
-    enable = true;
-    openFirewall = true;
-    streams = {
-      all = {
-        type = "meta";
-        location = "/turntable/spotify";
-      };
-      turntable = {
-        type = "tcp";
-        location = "0.0.0.0:49566";
-        query = {
-          sampleformat = "44100:16:2";
+      snapclient = {
+        wantedBy = [
+          "multi-user.target" # enable on boot
+        ];
+        after = [
+          "pipewire.service"
+        ];
+        serviceConfig = {
+          ExecStart = "${pkgs.snapcast}/bin/snapclient -h ::1";
         };
-      };
-      spotify = {
-        type = "librespot";
-        location = "${pkgs.librespot}/bin/librespot";
-        query = {
-          name = "spotify";
-          username = flake.email;
-          devicename = "home";
-          volume = "60";
-        };
-      };
-    };
-  };
-
-  systemd.services = {
-    snapserver.preStart = ''
-      export LIBRESPOT_PASSWORD="$(cat /run/secrets/spotify)"
-    '';
-    snapcast-sink = {
-      wantedBy = [ "pipewire.service" ];
-      after = [ "pipewire.service" ];
-      bindsTo = [ "pipewire.service" ];
-      path = with pkgs; [
-        gawk
-        pulseaudio
-      ];
-      script = ''
-        pactl load-module module-pipe-sink file=/run/snapserver/pipewire sink_name=Snapcast format=s16le rate=48000
-      '';
-    };
-
-    snapclient = {
-      wantedBy = [
-        "pipewire.service"
-        "multi-user.target" # enable on boot
-      ];
-      after = [
-        "pipewire.service"
-      ];
-      serviceConfig = {
-        ExecStart = "${pkgs.snapcast}/bin/snapclient -h ::1";
       };
     };
   };
