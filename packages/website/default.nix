4eb0d4d1402f5abf20289b58688bf9cd13c05b25
Bryton Hall <email@bryton.io>
2023-05-15 00:45:19 -0400

    packages(website): init from top-level directory

---
+++ packages/website/default.nix
@@ -0,0 +1,32 @@
+{ stdenv
+, hugo
+, fetchFromGitHub
+}:
+stdenv.mkDerivation rec {
+  name = "website";
+  src = ./.;
+
+  theme = fetchFromGitHub {
+    owner = "luizdepra";
+    repo = "hugo-coder";
+    rev = "d336b3c"; #"main";
+    sha256 = "SPYTWx9H0Y16ZEuoJcHXrq7Owe7QaR4/NmcOKx6kFYk=";
+  };
+
+  buildInputs = [
+    hugo # for use in a dev shell
+  ];
+
+  postUnpack = ''
+    mkdir -p ./website/themes
+    cp -r ${theme} ./website/themes/${theme.repo}
+  '';
+
+  buildPhase = ''
+    ${hugo}/bin/hugo
+  '';
+
+  installPhase = ''
+    cp -r ./public $out
+  '';
+}
