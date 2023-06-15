c61b41313fbc65d3a455183cebc5e97c5a97e01a
Bryton Hall <email@bryton.io>
2023-06-15 00:41:04 -0400

    esphome: add piano-keys device

--- cluster/esphome/livingroom.nix
+++ cluster/esphome/livingroom.nix
@@ -10,7 +10,7 @@
     light = [{
       platform = "neopixelbus";
       variant = "WS2812";
-      pin = "GPIO3"; # RX
+      pin = "GPIO3"; # "RX"
       num_leds = 146; # 26 x 47 rectangle
       name = "tv";
       method.type = "esp8266_dma";
@@ -18,5 +18,52 @@
         e131.universe = 1;
       }];
     }];
+  };
+  piano-keys = {
+    packages.wifi = "!include .wifi.yaml";
+    substitutions.name = "piano-keys";
+    esphome = {
+      platform = "ESP8266";
+      board = "nodemcuv2";
+      on_boot = [{
+        "light.turn_on" = {
+          id = "piano";
+          effect = "instruct";
+        };
+      }];
+    };
+    light = [{
+      name = "piano";
+      id = "piano";
+      num_leds = 88;
+      pin = "GPIO12"; # "D6"
+      platform = "neopixelbus";
+      variant = "WS2812";
+      effects = [{
+        addressable_lambda = {
+          name = "instruct";
+          update_interval = "50ms";
+          lambda = ''
+            while ((Serial.available()) > 0) {
+              int line = atoi(Serial.readStringUntil('\n').c_str());
+              Serial.println(line);
+              if(it[line].get().is_on()) {
+                it[line] = Color::BLACK;
+              } else {
+                it[line] = Color(50,50,50,50);
+              }
+            }
+          '';
+        };
+      }];
+    }];
+    # move logger off usb uart port
+    logger.hardware_uart = "UART1";
+    uart = {
+      id = "uart";
+      tx_pin = "GPIO1";
+      rx_pin = "GPIO3";
+      baud_rate = 9600;
+    };
   };
 }
