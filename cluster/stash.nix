ed08fd6b1a007d000e02954945fd2966f460e401
Bryton Hall <email@bryton.io>
2023-09-29 23:05:51 -0400

    stash: bump to 0.22.1

--- cluster/stash.nix
+++ cluster/stash.nix
@@ -2,17 +2,17 @@
   submodules.instances.stash = {
     submodule = "release";
     args = {
-      image = "stashapp/stash:v0.21.0";
+      image = "stashapp/stash:v0.22.1";
       port = 9999;
       host = "stash";
       persistence = {
         media = {
-          size = "50Gi";
+          size = "500Gi";
           mountPath = "/media";
           accessMode = "ReadWriteMany";
         };
         config = {
-          size = "10Gi";
+          size = "100Gi";
           mountPath = "/root/.stash";
         };
       };
