463e2782f93590d0c4e10b261097b8bf04adc5c4
Bryton Hall <email@bryton.io>
2023-10-21 12:15:35 -0400

    move syncthing from cluster to server

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -1,20 +1,24 @@
 { flake, lib, config, ... }:
 let
   ids = {
-    x12 = "FXVXWO2-GRRKEDH-3R6ZP3V-PPUYUTM-6FFHCNR-3YYRFJU-QOUHAMG-X27JJQB";
-    rigetti = "B5FZE7E-6HH43KW-BC2EI3A-FMORTYY-TOKOM66-Z7G5KX6-R4IIJXL-ULN6KQL";
-    pinephone = "NMTAKR7-KYODQLF-OFSZYNW-LCPHLRX-WNEX2IJ-LMFD5G2-TEMNVLB-SGK4ZQV";
-    server = "VHK4B2N-ISTNKQ5-PKSWYMN-OVFOIDF-EPC7DBG-GNTLB4J-ZHOU77D-JJB5YAW";
+    laptop = "FXVXWO2-GRRKEDH-3R6ZP3V-PPUYUTM-6FFHCNR-3YYRFJU-QOUHAMG-X27JJQB";
+    phone = "B7BBARA-YRPNSSW-VN25256-2MXN64T-WS5BHBJ-RYF32XI-6KRH4FB-QP2DAQQ";
+    server = "ANMYOD4-QYYMMN6-ZECPS5E-PM4F7BG-UUCBAVJ-QJS2R5C-5BMPN3W-HCLRSAB";
+    work = "B5FZE7E-6HH43KW-BC2EI3A-FMORTYY-TOKOM66-Z7G5KX6-R4IIJXL-ULN6KQL";
   };
 
   # only share with those in your group (names are arbitrary)
   groups = {
     personal = {
-      devices = [ "x12" "pinephone" "server" ];
-      folders = [ "notes" "cloud" "library" "sessions" ".stash" "photoprism" "paperless" ];
+      devices = [ "laptop" "server" ];
+      folders = [ "notes" "cloud" "library" "sessions" ".stash" ];
+    };
+    mobile = {
+      devices = [ "phone" ];
+      folders = [ "notes" ];
     };
     work = {
-      devices = [ "rigetti" ];
+      devices = [ "work" ];
       folders = [ "notes" ];
     };
   };
@@ -34,27 +38,31 @@ in
       user = flake.lib.username;
       openDefaultPorts = true;
       dataDir = home;
-      devices = builtins.listToAttrs (builtins.map
-        (device: {
-          name = device;
-          value.id = ids.${device};
-        })
-        (builtins.attrNames ids)
-        # (shared "devices")
-      );
 
-      folders = builtins.listToAttrs (builtins.map
-        (folder: {
-          name = folder;
-          value = {
-            path = "${home}/${folder}";
-            devices = builtins.concatMap (group: group.devices)
-              (builtins.filter (group: builtins.elem folder group.folders)
-                (builtins.attrValues groups));
-          };
-        })
-        (shared "folders")
-      );
+      settings = {
+        devices = builtins.listToAttrs (builtins.map
+          (device: {
+            name = device;
+            value.id = ids.${device};
+          })
+          (builtins.attrNames ids)
+          # (shared "devices")
+        );
+
+        folders = builtins.listToAttrs (builtins.map
+          (folder: {
+            name = folder;
+            value = {
+              path = "${home}/${folder}";
+              devices = builtins.concatMap (group: group.devices)
+                (builtins.filter (group: builtins.elem folder group.folders)
+                  (builtins.attrValues groups));
+            };
+          })
+          (shared "folders")
+        );
+      };
+
     };
   };
 }
