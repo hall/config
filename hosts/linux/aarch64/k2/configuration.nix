f6e6cfade989a7cd40cd80b934a87ad0230db647
Bryton Hall <email@bryton.io>
2023-10-22 01:06:41 -0400

    remove all cluster nodes

--- hosts/linux/aarch64/k2/configuration.nix
+++
@@ -1,7 +0,0 @@
-{ flake, ... }: {
-  imports = [ flake.inputs.hardware.nixosModules.raspberry-pi-4 ];
-  services.k8s = {
-    enable = true;
-    role = "server";
-  };
-}
--- hosts/linux/aarch64/k5/configuration.nix
+++
@@ -1,7 +0,0 @@
-{ flake, modulesPath, ... }: {
-  imports = [
-    flake.inputs.hardware.nixosModules.raspberry-pi-4
-    (modulesPath + "/installer/sd-card/sd-image-aarch64.nix")
-  ];
-  services.k8s.enable = true;
-}
