b3bccb2d37170cc246b361124066c2291ba35d6c
Bryton Hall <email@bryton.io>
2023-11-16 01:55:20 -0500

    hosts(laptop): add adguard, stash, syncthing

--- hosts/linux/x86_64/server/configuration.nix
+++ hosts/linux/x86_64/server/configuration.nix
@@ -1,16 +1,29 @@
 # NUC11PAHi7, 88:AE:DD:05:C9:46, F2 to enter bios
+# TODO
+# - circadian lighting
+# - connect
+#   - switches: https://www.indiegogo.com/projects/mmwave-smart-switch-with-presence-sensing-radar
+#   - led controllers
+#   - konnected: https://www.reddit.com/r/konnected/comments/15pom5f/any_update_on_matter_support/
+#   - power monitoring?
+#   - mailbox?
 { lib, pkgs, flake, config, ... }: {
   imports = [ flake.inputs.hardware.nixosModules.intel-nuc-8i7beh ];
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
-  services.wifi.enable = true;
-
   networking = {
-    firewall.allowedTCPPorts = [
-      80 # http redirect
-      443 # https
-      # 1883 # mqtt
-    ];
+    firewall = {
+      allowedTCPPorts = [
+        80 # http redirect
+        443 # https
+        53 # dns fallback
+      ];
+      allowedUDPPorts = [
+        53 # dns
+        67 # dhcp
+        68 # dhcp
+      ];
+    };
   };
 
   age.secrets.namecheap.file = ../../../../secrets/namecheap.age;
@@ -18,50 +31,89 @@
     acceptTerms = true;
     defaults = {
       email = flake.lib.email;
+      group = "nginx";
       dnsProvider = "namecheap";
       credentialsFile = "/run/secrets/namecheap";
     };
-    certs."${flake.lib.hostname}" = {
-      domain = "*.${flake.lib.hostname}";
-    };
+    certs."${flake.lib.hostname}".domain = "*.${flake.lib.hostname}";
   };
 
   services = {
+    wifi.enable = true;
 
-    nginx = {
+    adguardhome = {
       enable = true;
-      virtualHosts = {
-        "esphome.${flake.lib.hostname}" = {
-          enableACME = true;
-          acmeRoot = null;
-          locations."/".proxyPass = "http://127.0.0.1:${builtins.toString config.services.esphome.port}";
-          # locations."/".proxyPass = "http://127.0.0.1:${config.services.esphome.port}";
+      # mutableSettings = false;
+      settings = {
+        bind_host = "127.0.0.1";
+        # bootstrap_dns
+        filtering.rewrites = [{
+          domain = "*.${flake.lib.hostname}";
+          answer = "10.0.0.2";
+        }];
+        dhcp = {
+          enabled = true;
+          interface_name = "wlo1";
+          dhcpv4 = {
+            gateway_ip = "10.0.0.1";
+            subnet_mask = "255.255.255.0";
+            range_start = "10.0.0.10";
+            range_end = "10.0.0.254";
+          };
         };
       };
     };
 
-    esphome.enable = true;
+    nginx = {
+      enable = true;
+      # recommendedGzipSettings = true;
+      # recommendedOptimisation = true;
+      # recommendedProxySettings = true;
+      # recommendedTlsSettings = true;
 
-    mosquitto = {
-      # /var/lib/mosquitto
-      # enable = true;
-      settings = {
-        listener = 1883;
-        allow_anonymous = true;
-        connection_messages = false;
+      virtualHosts = {
+        "adguard.${flake.lib.hostname}" = {
+          useACMEHost = flake.lib.hostname;
+          acmeRoot = null;
+          forceSSL = true;
+          locations."/".proxyPass = "http://127.0.0.1:3000";
+        };
+        "sync.${flake.lib.hostname}" = {
+          useACMEHost = flake.lib.hostname;
+          acmeRoot = null;
+          forceSSL = true;
+          locations."/".proxyPass = "http://${builtins.toString config.services.syncthing.guiAddress}";
+          extraConfig = ''
+            proxy_read_timeout      600s;
+            proxy_send_timeout      600s;
+          '';
+        };
+        "stash.${flake.lib.hostname}" = {
+          useACMEHost = flake.lib.hostname;
+          acmeRoot = null;
+          forceSSL = true;
+          locations."/".proxyPass = "http://127.0.0.1:${builtins.toString config.services.stash.port}";
+          extraConfig = ''
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "upgrade";
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_read_timeout 60000s;
+          '';
+        };
       };
     };
-    zigbee2mqtt = {
-      # /var/lib/zigbee2mqtt
-      # enable = true;
-      # settings = {
-      #   homeassistant = config.services.home-assistant.enable;
-      #   permit_join = true;
-      #   serial = {
-      #     port = "/dev/ttyACM1";
-      #   };
-      # };
+
+    stash = {
+      # /var/lib/stash  # app data
+      enable = true;
+      group = "syncthing";
     };
+
+    syncthing.settings.folders.stash.path = lib.mkForce "/var/lib/stash";
 
     iperf3 = {
       enable = true;
@@ -69,6 +121,9 @@
     };
   };
 
-  environment.systemPackages = with pkgs; [ iperf ];
+  environment.systemPackages = with pkgs; [
+    iperf
+    htop
+  ];
 
 }
