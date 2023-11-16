9502599abd038fa351f342081ab6d1ef3ba0683f
Bryton Hall <email@bryton.io>
2023-11-16 01:46:35 -0500

    modules(syncthing): rotate phone id

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -2,7 +2,7 @@
 let
   ids = {
     laptop = "FXVXWO2-GRRKEDH-3R6ZP3V-PPUYUTM-6FFHCNR-3YYRFJU-QOUHAMG-X27JJQB";
-    phone = "B7BBARA-YRPNSSW-VN25256-2MXN64T-WS5BHBJ-RYF32XI-6KRH4FB-QP2DAQQ";
+    phone = "LGVEZX6-TYKLGEY-JJWU3ND-Y7TWKWX-U4FHOAG-MY2PK3J-3YQW3HI-BYADJAX";
     server = "ANMYOD4-QYYMMN6-ZECPS5E-PM4F7BG-UUCBAVJ-QJS2R5C-5BMPN3W-HCLRSAB";
     work = "B5FZE7E-6HH43KW-BC2EI3A-FMORTYY-TOKOM66-Z7G5KX6-R4IIJXL-ULN6KQL";
   };
@@ -11,11 +11,11 @@ let
   groups = {
     personal = {
       devices = [ "laptop" "server" ];
-      folders = [ "notes" "cloud" "library" "sessions" ".stash" ];
+      folders = [ "notes" "library" "sessions" "stash" ];
     };
     mobile = {
       devices = [ "phone" ];
-      folders = [ "notes" ];
+      folders = [ "notes" "stash" ];
     };
     work = {
       devices = [ "work" ];
