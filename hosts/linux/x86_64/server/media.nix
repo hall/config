e35a2d1f807ddfa018168edcf4afc7d59145a790
Bryton Hall <email@bryton.io>
2024-08-11 02:03:25 -0400

    hosts(server): add jellyseerr

--- hosts/linux/x86_64/server/media.nix
+++ hosts/linux/x86_64/server/media.nix
@@ -1,19 +1,8 @@
-{ flake, pkgs, ... }: {
-
-  # nixarr = {
-  #   enable = true;
-  #   transmission = {
-  #     enable = true;
-  #     extraSettings = { };
-  #   };
-  # };
-
+{ flake, pkgs, config, ... }: {
   services = {
     jellyfin = {
       enable = true;
       user = flake.lib.username;
-      # group = "syncthing";
-      # openFirewall = true;
     };
     nginx.virtualHosts = {
       "player.${flake.lib.hostname}" = {
@@ -24,16 +13,23 @@
       };
     };
 
-      # enable = true;
+    jellyseerr.enable = true;
+    nginx.virtualHosts = {
+      "requests.${flake.lib.hostname}" = {
+        useACMEHost = flake.lib.hostname;
+        acmeRoot = null;
+        forceSSL = true;
+        locations."/".proxyPass = "http://127.0.0.1:${toString config.services.jellyseerr.port}";
+      };
+    };
+
     transmission = {
+      enable = true;
       package = pkgs.transmission_4;
       user = flake.lib.username;
-      # group = "syncthing";
-      # openFirewall = true;
       webHome = pkgs.flood-for-transmission;
       settings = {
-        # download-dir = "/var/lib/media/downloads";
-
+        download-dir = "/var/lib/media/downloads";
       };
     };
     nginx.virtualHosts = {
@@ -41,15 +37,13 @@
         useACMEHost = flake.lib.hostname;
         acmeRoot = null;
         forceSSL = true;
-        locations."/".proxyPass = "http://127.0.0.1:9091";
+        locations."/".proxyPass = "http://127.0.0.1:${toString config.services.transmission.settings.rpc-port}";
       };
     };
 
     sonarr = {
       enable = true;
       user = flake.lib.username;
-      # group = "syncthing";
-      # openFirewall = true;
     };
     nginx.virtualHosts = {
       "shows.${flake.lib.hostname}" = {
@@ -57,17 +51,12 @@
         acmeRoot = null;
         forceSSL = true;
         locations."/".proxyPass = "http://127.0.0.1:8989";
-        # proxy_http_version 1.1;
-        # proxy_set_header Upgrade $http_upgrade;
-        # proxy_set_header Connection $http_connection;
       };
     };
 
     radarr = {
       enable = true;
       user = flake.lib.username;
-      # group = "syncthing";
-      # openFirewall = true;
     };
     nginx.virtualHosts = {
       "movies.${flake.lib.hostname}" = {
@@ -81,8 +70,6 @@
     lidarr = {
       enable = true;
       user = flake.lib.username;
-      # group = "syncthing";
-      # openFirewall = true;
     };
     nginx.virtualHosts = {
       "music.${flake.lib.hostname}" = {
@@ -96,8 +83,6 @@
     prowlarr = {
       enable = true;
       # user = flake.lib.username;
-      # group = "syncthing";
-      # openFirewall = true;
     };
     nginx.virtualHosts = {
       "indexer.${flake.lib.hostname}" = {
@@ -108,9 +93,4 @@
       };
     };
   };
-  environment.systemPackages = [
-    pkgs.jellyfin
-    pkgs.jellyfin-web
-    pkgs.jellyfin-ffmpeg
-  ];
 }
