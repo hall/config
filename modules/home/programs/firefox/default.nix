6141bee6b5feb69152fb26b663252f8e58099d0c
Bryton Hall <email@bryton.io>
2024-09-24 22:32:54 -0400

    firefox: use 3rd-party css hacks

--- modules/home/programs/firefox/default.nix
+++ modules/home/programs/firefox/default.nix
@@ -1,4 +1,4 @@
-{ config, ... }: {
+{ lib, pkgs, ... }: {
   enable = true;
   policies = {
     ExtensionSettings = (builtins.mapAttrs
@@ -30,128 +30,89 @@
     DisableSetDesktopBackground = true;
     DisablePocket = true;
   };
-  profiles.default = {
-    userChrome = builtins.readFile ./userChrome.css;
-    userContent = builtins.readFile ./userContent.css;
-    search = {
-      default = "DuckDuckGo";
-      force = true; # prevent ff from overwriting
+  profiles.default =
+    let
+      csshacks = pkgs.fetchFromGitHub {
+        owner = "MrOtherGuy";
+        repo = "firefox-csshacks";
+        rev = "master";
+        hash = "sha256-22HUmpBZm3E5dN+kgdj4S3qFtZrIvTbz5svkCt1+Ruo=";
+      };
+    in
+    {
+      userChrome = lib.strings.concatMapStrings builtins.readFile [
+        (csshacks + /chrome/autohide_sidebar.css)
+        (csshacks + /chrome/urlbar_info_icons_on_hover.css)
+        (csshacks + /chrome/page_action_buttons_on_hover.css)
+        ./userChrome.css
+      ];
+      userContent = builtins.readFile ./userContent.css;
+      search = {
+        default = "DuckDuckGo";
+        force = true; # prevent ff from overwriting
+      };
+      containersForce = true;
+      containers = {
+        github = {
+          id = 1;
+          name = "github";
+          color = "blue";
+          icon = "circle";
+        };
+        google = {
+          id = 2;
+          name = "google";
+          color = "blue";
+          icon = "circle";
+        };
+        work = {
+          id = 3;
+          name = "work";
+          color = "green";
+          icon = "circle";
+        };
+        amazon = {
+          id = 4;
+          name = "amazon";
+          color = "orange";
+          icon = "circle";
+        };
+      };
+      # extensions = with config.nur.repos.rycee.firefox-addons; [
+      #   # anchors-reveal
+      #   # auto-tab-discard
+      #   buster-captcha-solver
+      #   # bypass-paywalls-clean
+      #   firefox-translations
+      #   gsconnect
+      #   i-dont-care-about-cookies
+      #   link-cleaner
+      # ];
+      settings = {
+        "browser.aboutConfig.showWarning" = false;
+        "browser.display.use_system_colors" = true;
+        "browser.newtabpage.enabled" = false;
+        "browser.search.defaultenginename" = "duckduckgo";
+        "browser.startup.homepage" = "about:blank";
+        "browser.startup.page" = 3; # restore previous session
+        "browser.tabs.drawInTitlebar" = false;
+        "browser.toolbars.bookmarks.visibility" = "never";
+        # use theme for browser content and toolbar
+        "browser.theme.content-theme" = 0;
+        "browser.theme.toolbar-theme" = 0;
+        "browser.uidensity" = 1; # compact
+        "extensions.pocket.enabled" = false;
+        "geo.enabled" = false;
+        "print.tab_modal.enabled" = false;
+        "signon.rememberSignons" = false;
+        "svg.context-properties.content.enabled" = true;
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "browser.tabs.warnOnCloseOtherTabs" = false;
+        # auto-enable extensions
+        "extensions.autoDisableScopes" = 0;
+        # don't allow sites to steal keyboard input
+        # TODO: breaks tridactyl `esc` :/
+        # "permissions.default.shortcuts" = 2;
+      };
     };
-    containersForce = true;
-    containers = {
-      github = {
-        id = 1;
-        name = "github";
-        color = "blue";
-        icon = "circle";
-      };
-      google = {
-        id = 2;
-        name = "google";
-        color = "blue";
-        icon = "circle";
-      };
-      work = {
-        id = 3;
-        name = "work";
-        color = "green";
-        icon = "circle";
-      };
-      amazon = {
-        id = 4;
-        name = "amazon";
-        color = "orange";
-        icon = "circle";
-      };
-    };
-    # extensions = with config.nur.repos.rycee.firefox-addons; [
-    #   # anchors-reveal
-    #   # auto-tab-discard
-    #   buster-captcha-solver
-    #   # bypass-paywalls-clean
-    #   firefox-translations
-    #   gsconnect
-    #   i-dont-care-about-cookies
-    #   link-cleaner
-    # ];
-    settings = {
-      "browser.aboutConfig.showWarning" = false;
-      "browser.display.use_system_colors" = true;
-      "browser.newtabpage.enabled" = false;
-      "browser.search.defaultenginename" = "duckduckgo";
-      "browser.startup.homepage" = "about:blank";
-      "browser.startup.page" = 3; # restore previous session
-      "browser.tabs.drawInTitlebar" = false;
-      "browser.toolbars.bookmarks.visibility" = "never";
-      # use theme for browser content and toolbar
-      "browser.theme.content-theme" = 0;
-      "browser.theme.toolbar-theme" = 0;
-      "browser.uidensity" = 1; # compact
-      "extensions.pocket.enabled" = false;
-      "geo.enabled" = false;
-      "print.tab_modal.enabled" = false;
-      "signon.rememberSignons" = false;
-      "svg.context-properties.content.enabled" = true;
-      "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-      "browser.tabs.warnOnCloseOtherTabs" = false;
-      # auto-enable extensions
-      "extensions.autoDisableScopes" = 0;
-      # don't allow sites to steal keyboard input
-      # TODO: breaks tridactyl `esc` :/
-      # "permissions.default.shortcuts" = 2;
-
-      # customize the toolbar
-      # TODO: can this be cleaned up?
-      # "browser.uiCustomization.state" = {
-      #   placements = {
-      #     widget-overflow-fixed-list = [ ];
-      #     unified-extensions-area = [
-      #       "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action" # bitwarden
-      #       "ublock0_raymondhill_net-browser-action"
-      #       "_3c078156-979c-498b-8990-85f7987dd929_-browser-action" # sidebery
-      #       "_278b0ae0-da9d-4cc6-be81-5aa7f3202672_-browser-action" # allow-right-click
-      #       "addon_darkreader_org-browser-action"
-      #       "_testpilot-containers-browser-action"
-      #     ];
-      #     nav-bar = [
-      #       "back-button"
-      #       "forward-button"
-      #       "stop-reload-button"
-      #       "urlbar-container"
-      #       "downloads-button"
-      #       "unified-extensions-button"
-      #       "fxa-toolbar-menu-button"
-      #     ];
-      #     toolbar-menubar = [ "menubar-items" ];
-      #     TabsToolbar = [
-      #       "firefox-view-button"
-      #       "tabbrowser-tabs"
-      #       "new-tab-button"
-      #       "alltabs-button"
-      #       "_c607c8df-14a7-4f28-894f-29e8722976af_-browser-action"
-      #     ];
-      #     PersonalToolbar = [ "import-button" "personal-bookmarks" ];
-      #   };
-      #   seen = [
-      #     "developer-button"
-      #     "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action"
-      #     "ublock0_raymondhill_net-browser-action"
-      #     "_3c078156-979c-498b-8990-85f7987dd929_-browser-action"
-      #     "_278b0ae0-da9d-4cc6-be81-5aa7f3202672_-browser-action"
-      #     "addon_darkreader_org-browser-action"
-      #     "_c607c8df-14a7-4f28-894f-29e8722976af_-browser-action"
-      #     "_testpilot-containers-browser-action"
-      #   ];
-      #   dirtyAreaCache = [
-      #     "nav-bar"
-      #     "unified-extensions-area"
-      #     "PersonalToolbar"
-      #     "toolbar-menubar"
-      #     "TabsToolbar"
-      #   ];
-      #   currentVersion = 19;
-      #   newElementCount = 2;
-      # };
-    };
-  };
 }
