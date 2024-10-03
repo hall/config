14990802f997eb0a1b5864e85a9d0d0266072010
Bryton Hall <email@bryton.io>
2024-10-03 01:04:25 -0400

    monitoring: breakup alloy config, add hass integration

--- hosts/linux/x86_64/server/monitoring.nix
+++ hosts/linux/x86_64/server/monitoring.nix
@@ -9,100 +9,109 @@
       "--disable-reporting"
     ];
   };
-  environment.etc."alloy/config.alloy".text = ''
-    logging {
-      level = "warn"
-    }
-
-    tracing {
-      sampling_fraction = 0.1
-      write_to = [otelcol.exporter.otlp.default.input]
-    }
-
-    otelcol.exporter.otlp "default" {
-      client {
-        endpoint = "tempo-prod-04-prod-us-east-0.grafana.net:443"
-        auth = otelcol.auth.basic.default.handler
+  environment.etc = {
+    "alloy/config.alloy".text = ''
+      logging {
+        level = "warn"
       }
-    }
 
-    otelcol.auth.basic "default" {
-      username = sys.env("OTELCOL_USERNAME")
-      password = sys.env("GRAFANA_PASSWORD")
-    }
-    
-    prometheus.remote_write "default" {
-      endpoint {
-        url = "https://prometheus-prod-13-prod-us-east-0.grafana.net/api/prom/push"
-        basic_auth {
-          username = sys.env("PROMETHEUS_USERNAME")
-          password = sys.env("GRAFANA_PASSWORD")
+      tracing {
+        sampling_fraction = 0.1
+        write_to = [otelcol.exporter.otlp.default.input]
+      }
+    '';
+
+    "alloy/metrics.alloy".text = ''
+      prometheus.remote_write "default" {
+        endpoint {
+          url = "https://prometheus-prod-13-prod-us-east-0.grafana.net/api/prom/push"
+          basic_auth {
+            username = sys.env("PROMETHEUS_USERNAME")
+            password = sys.env("GRAFANA_PASSWORD")
+          }
         }
       }
-    }
 
-    prometheus.relabel "all" {
-      forward_to = [prometheus.remote_write.default.receiver]
-      rule {
-        action       = "replace"
-        target_label = "instance"
-        replacement  = constants.hostname
-      }
-    }
-
-    prometheus.exporter.unix "localhost" {}
-
-    prometheus.scrape "node" {
-      forward_to = [prometheus.relabel.all.receiver]
-      targets    = prometheus.exporter.unix.localhost.targets
-    }
-
-    loki.write "default" {
-      endpoint {
-        url = "https://logs-prod-006.grafana.net/loki/api/v1/push"
-    
-        basic_auth {
-          username = sys.env("LOKI_USERNAME")
-          password = sys.env("GRAFANA_PASSWORD")
+      prometheus.relabel "all" {
+        forward_to = [prometheus.remote_write.default.receiver]
+        rule {
+          action       = "replace"
+          target_label = "instance"
+          replacement  = constants.hostname
         }
       }
-    }
 
-    loki.relabel "journal" {
-      forward_to = []
-      rule {
-        source_labels = ["__journal__systemd_unit"]
-        target_label  = "unit"
+      prometheus.exporter.unix "localhost" {}
+
+      prometheus.scrape "node" {
+        forward_to = [prometheus.relabel.all.receiver]
+        targets    = prometheus.exporter.unix.localhost.targets
       }
-    }
+    '';
 
-    loki.source.journal "default" {
-      forward_to    = [loki.write.default.receiver]
-      relabel_rules = loki.relabel.journal.rules
-      labels        = { instance = constants.hostname }
-    }
+    "alloy/logs.alloy".text = ''
+      loki.write "default" {
+        endpoint {
+          url = "https://logs-prod-006.grafana.net/loki/api/v1/push"
+    
+          basic_auth {
+            username = sys.env("LOKI_USERNAME")
+            password = sys.env("GRAFANA_PASSWORD")
+          }
+        }
+      }
 
-    discovery.relabel "metrics_integrations_integrations_hass" {
-    	targets = [{
-    		__address__ = "localhost:8123",
-    	}]
-    	rule {
-    		target_label = "instance"
-    		replacement  = constants.hostname
-    	}
-    }
+      loki.relabel "journal" {
+        forward_to = []
+        rule {
+          source_labels = ["__journal__systemd_unit"]
+          target_label  = "unit"
+        }
+      }
+
+      loki.source.journal "default" {
+        forward_to    = [loki.write.default.receiver]
+        relabel_rules = loki.relabel.journal.rules
+        labels        = { instance = constants.hostname }
+      }
+    '';
+
+    "alloy/traces.alloy".text = ''
+      otelcol.exporter.otlp "default" {
+        client {
+          endpoint = "tempo-prod-04-prod-us-east-0.grafana.net:443"
+          auth = otelcol.auth.basic.default.handler
+        }
+      }
+
+      otelcol.auth.basic "default" {
+        username = sys.env("OTELCOL_USERNAME")
+        password = sys.env("GRAFANA_PASSWORD")
+      }
+    '';
+
+    "alloy/home-assistant.alloy".text = ''
+      discovery.relabel "metrics_integrations_integrations_hass" {
+      	targets = [{
+      		__address__ = "localhost:8123",
+      	}]
+      	rule {
+      		target_label = "instance"
+      		replacement  = constants.hostname
+      	}
+      }
     
-    prometheus.scrape "metrics_integrations_integrations_hass" {
-    	targets      = discovery.relabel.metrics_integrations_integrations_hass.output
-    	forward_to   = [prometheus.remote_write.default.receiver]
-    	job_name     = "integrations/hass"
-    	metrics_path = "/api/prometheus"
-    	scheme       = "https"
-    
-    	authorization {
-    		type        = "Bearer"
-    		credentials = sys.env("HASS_TOKEN")
-    	}
-    }
-  '';
+      prometheus.scrape "metrics_integrations_integrations_hass" {
+      	targets      = discovery.relabel.metrics_integrations_integrations_hass.output
+      	forward_to   = [prometheus.remote_write.default.receiver]
+      	job_name     = "integrations/hass"
+      	metrics_path = "/api/prometheus"
+      	scheme       = "http"
+      	authorization {
+      		type        = "Bearer"
+      		credentials = sys.env("HASS_TOKEN")
+      	}
+      }
+    '';
+  };
 }
