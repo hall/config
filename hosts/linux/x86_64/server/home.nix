d3087ad6fe8788901e37201f5db9993c06c761b5
Bryton Hall <email@bryton.io>
2024-10-14 09:58:55 -0400

    hosts(server): add mosquitto instance

--- hosts/linux/x86_64/server/home.nix
+++ hosts/linux/x86_64/server/home.nix
@@ -109,6 +109,15 @@
       };
     };
 
+    mosquitto = {
+      enable = true;
+      listeners = [{
+        acl = [ "pattern readwrite #" ];
+        omitPasswordAuth = true;
+        settings.allow_anonymous = true;
+      }];
+    };
+
     postgresql = {
       enable = true;
       ensureDatabases = [ "hass" ];
