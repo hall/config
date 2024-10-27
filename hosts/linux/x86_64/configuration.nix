9033ffee3430d796849e0e7a062a720d898ef7cd
Bryton Hall <email@bryton.io>
2024-10-27 16:55:23 -0400

    hosts: remove redundant disko config

--- hosts/linux/x86_64/configuration.nix
+++ hosts/linux/x86_64/configuration.nix
@@ -1,5 +1,9 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 {
+  imports = [
+    ./disks.nix
+  ];
+
   boot = {
     consoleLogLevel = 0;
     loader = {
@@ -19,59 +23,5 @@
   # powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = true;
 
-  disko.devices = {
-    disk.main = {
-      type = "disk";
-      device = "/dev/nvme0n1";
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            size = "500M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-            };
-          };
-          luks = {
-            size = "100%";
-            content = {
-              type = "luks";
-              name = "crypt";
-              additionalKeyFiles = [ config.age.secrets.luks.path ];
-              settings = {
-                # dd if=/dev/random of=/dev/sda bs=2048 count=1
-                keyFile = "/dev/sda";
-                keyFileSize = 2048;
-                fallbackToPassword = true;
-              };
-              content = {
-                type = "lvm_pv";
-                vg = "pool";
-              };
-            };
-          };
-        };
-      };
-    };
-    lvm_vg.pool = {
-      type = "lvm_vg";
-      lvs = {
-        swap = {
-          size = "16G";
-          content.type = "swap";
-        };
-        root = {
-          size = "100%FREE";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
-          };
-        };
-      };
-    };
-  };
+  systemd.services.tailscaled.after = [ "NetworkManager-wait-online.service" ];
 }
