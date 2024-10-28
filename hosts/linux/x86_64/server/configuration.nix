7ae84f1ebb51a4b7582f0831389dfe3f77136745
Bryton Hall <email@bryton.io>
2024-10-27 23:46:13 -0400

    hosts(server): remove old vaapi driver

--- hosts/linux/x86_64/server/configuration.nix
+++ hosts/linux/x86_64/server/configuration.nix
@@ -26,7 +26,6 @@
       enable = true;
       extraPackages = with pkgs; [
         intel-media-driver # LIBVA_DRIVER_NAME=iHD
-        intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
         vaapiVdpau
         intel-compute-runtime
         vpl-gpu-rt
