e69faee6664f217e79dcc753ce9153bd35c3efe6
Bryton Hall <email@bryton.io>
2025-01-01 01:20:20 -0500

    hosts(server): add mqtt and zigbee

--- hosts/linux/x86_64/server/home/mqtt.nix
+++ hosts/linux/x86_64/server/home/mqtt.nix
@@ -1,10 +1,52 @@
-{ ... }: {
-  services.mosquitto = {
-    enable = true;
-    listeners = [{
-      acl = [ "pattern readwrite #" ];
-      omitPasswordAuth = true;
-      settings.allow_anonymous = true;
-    }];
+{ flake, ... }: {
+  services = {
+    mosquitto = {
+      enable = true;
+      listeners = [{
+        acl = [ "pattern readwrite #" ];
+        omitPasswordAuth = true;
+        settings = {
+          allow_anonymous = true;
+          # keepalive = 15;
+          # version = "5";
+        };
+      }];
+    };
+    nginx.virtualHosts."mqtt.${flake.lib.hostname}" = {
+      useACMEHost = flake.lib.hostname;
+      acmeRoot = null;
+      forceSSL = true;
+      extraConfig = ''
+        proxy_buffering off;
+      '';
+      locations."/" = {
+        proxyPass = "http://[::1]:1883";
+        proxyWebsockets = true;
+      };
+    };
+
+    zigbee2mqtt = {
+      enable = true;
+      settings = {
+        frontend = true;
+        home-assistant = true;
+        serial = {
+          # TODO: centralize static addresses
+          port = "tcp://192.168.86.11:6638";
+        };
+      };
+    };
+    nginx.virtualHosts."zigbee.${flake.lib.hostname}" = {
+      useACMEHost = flake.lib.hostname;
+      acmeRoot = null;
+      forceSSL = true;
+      extraConfig = ''
+        proxy_buffering off;
+      '';
+      locations."/" = {
+        proxyPass = "http://[::1]:8080";
+        proxyWebsockets = true;
+      };
+    };
   };
 }
