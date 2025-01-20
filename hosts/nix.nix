b3ee61974d9b4263bf350aa795db0bf7817692cf
Bryton Hall <email@bryton.io>
2025-01-19 19:18:03 -0500

    nix: add insecure packages and cache.nixos.org pubkey

--- hosts/nix.nix
+++ hosts/nix.nix
@@ -24,6 +24,8 @@
 
     permittedInsecurePackages = [
       "electron-27.3.11"
+      "dotnet-sdk-6.0.428" # sonarr
+      "aspnetcore-runtime-6.0.36" # sonarr
     ];
   };
 
@@ -44,6 +46,7 @@
       experimental-features = "nix-command flakes";
       trusted-users = [ "root" flake.lib.username ];
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
