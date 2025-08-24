fdd2ff837b63c4273bc0bfca8a0c14645816a6a5
Bryton Hall <email@bryton.io>
2025-08-24 13:06:21 -0400

    stylix: switch to catppuccin frappe

--- modules/gui/default.nix
+++ modules/gui/default.nix
@@ -22,24 +22,7 @@ in
 
     stylix = {
       enable = true;
-      base16Scheme.palette = {
-        base00 = "#2b303b";
-        base01 = "#343d46"; # TODO: secondary bg color, needs to be a little less green?
-        base02 = "#4f5b66";
-        base03 = "#65737e";
-        base04 = "#a7adba";
-        base05 = "#c0c5ce";
-        base06 = "#dfe1e8";
-        base07 = "#eff1f5";
-        base08 = "#bf616a";
-        base09 = "#d08770";
-        base0A = "#ebcb8b";
-        base0B = "#a3be8c";
-        base0C = "#96b5b4";
-        base0D = "#8fa1b3";
-        base0E = "#b48ead";
-        base0F = "#ab7967";
-      };
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-frappe.yaml";
       fonts = {
         serif = config.stylix.fonts.monospace;
         sansSerif = config.stylix.fonts.monospace;
