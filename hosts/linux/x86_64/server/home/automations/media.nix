5c2ced83aa9d8c617661ad05d7f383c41e0ad163
Bryton Hall <email@bryton.io>
2025-11-02 19:35:26 -0500

    hass: improve media notifications

--- hosts/linux/x86_64/server/home/automations/media.nix
+++ hosts/linux/x86_64/server/home/automations/media.nix
@@ -10,7 +10,22 @@
     service = "notify.phones";
     data = {
       title = "new media";
-      message = "{{ trigger.json.get('Name', 'Unknown') }}";
+      message = ''
+        {% if trigger.json.get('ItemType') == 'Episode' and trigger.json.get('SeriesName') %}
+        {{ trigger.json.SeriesName }}{% if trigger.json.get('SeasonNumber00') and trigger.json.get('EpisodeNumber00') %} - S{{ trigger.json.SeasonNumber00 }}E{{ trigger.json.EpisodeNumber00 }}{% endif %}{% if trigger.json.get('Name') %}: {{ trigger.json.Name }}{% endif %}
+        {% elif trigger.json.get('ItemType') == 'Movie' %}
+        {{ trigger.json.get('Name', 'Unknown') }}{% if trigger.json.get('Year') %} ({{ trigger.json.Year }}){% endif %}
+        {% elif trigger.json.get('ItemType') == 'Book' %}
+        {{ trigger.json.get('Name', 'Unknown') }}{% if trigger.json.get('Year') %} ({{ trigger.json.Year }}){% endif %}
+        {% elif trigger.json.get('ItemType') == 'Audio' %}
+        {{ trigger.json.get('Name', 'Unknown') }}{% if trigger.json.get('Album') %} - {{ trigger.json.Album }}{% endif %}
+        {% else %}
+        {{ trigger.json.get('Name', 'Unknown') }}
+        {% endif %}
+      '';
+      data = {
+        image = "{{ trigger.json.get('ImageUrl', '') }}";
+      };
     };
   }];
 }
